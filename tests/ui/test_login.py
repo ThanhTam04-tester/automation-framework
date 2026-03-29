b7c8bb15d25fe9d4bf1a00c82f70585a688b0d75
Tam <maithimeo2004@gmail.com>
2026-03-29 15:09:22 +0700

    Sua URL trong test_login thanh host.docker.internal

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 @pytest.mark.ui
 def test_login(driver):
-    driver.get("http://127.0.0.1:5000/login")
+    driver.get("http://host.docker.internal:5000/login")
 
     driver.find_element(By.NAME, "username").send_keys("admin")
     driver.find_element(By.NAME, "password").send_keys("123")
