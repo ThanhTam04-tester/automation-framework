99fa8065dda19a647009356de32afc50038d2800
mthnhtam04-star <mthnh.tam04@gmail.com>
2026-05-14 11:08:55 +0700

    fix demoqa click intercepted 4

--- tests/demoqa/test_demoqa.py
+++ tests/demoqa/test_demoqa.py
@@ -3,14 +3,15 @@ import allure
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 @allure.epic("Cross-Project Testing")
 @allure.feature("DemoQA Practice Form")
-
 class TestDemoQA:
 
     @allure.title("TC_01: Truy cập thành công trang DemoQA")
-
     def test_open_demoqa(self, driver):
 
         target_url = "https://demoqa.com/"
@@ -18,23 +19,27 @@ class TestDemoQA:
         with allure.step(f"1. Truy cập website: {target_url}"):
 
             driver.get(target_url)
-            time.sleep(2)
 
         with allure.step("2. Xác minh website mở thành công"):
 
             # Kiểm tra URL hiện tại
             assert "demoqa.com" in driver.current_url
 
-            # Kiểm tra logo/trang chủ tồn tại
-            logo = driver.find_elements(By.CLASS_NAME, "home-banner")
+            # Kiểm tra banner trang chủ tồn tại
+            logo = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "home-banner")
+                )
+            )
 
-            assert len(logo) > 0, "Không tìm thấy banner trang chủ DemoQA!"
+            assert logo.is_displayed()
 
         with allure.step("3. Click vào mục Elements"):
 
-            elements_card = driver.find_element(
-                By.XPATH,
-                "//h5[text()='Elements']"
+            elements_card = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//h5[text()='Elements']")
+                )
             )
 
             # Scroll tới element
@@ -51,17 +56,16 @@ class TestDemoQA:
                 elements_card
             )
 
-            time.sleep(2)
-
         with allure.step("4. Xác minh đã chuyển sang trang Elements"):
 
-            assert "elements" in driver.current_url
-
-            header = driver.find_element(
-                By.CLASS_NAME,
-                "main-header"
+            # Đợi header xuất hiện
+            header = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located(
+                    (By.CLASS_NAME, "main-header")
+                )
             )
 
+            assert "elements" in driver.current_url
             assert header.text == "Elements"
 
         with allure.step("5. Chụp ảnh màn hình minh chứng"):
